05e08a38bb9837848cad00755fb0001f2b8af2c1
Juan José García Duarte <gdjuanjojr@gmail.com>
2026-08-08 22:56:49 +0200

    feat(schelling): Randomize empty room choice
    To be improved with other data structure

--- src/schelling/SchellingModel.java
+++ src/schelling/SchellingModel.java
@@ -4,38 +4,40 @@ import game.Cell;
 import game.Game;
 
 import java.awt.Color;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import game.Grid;
 import schelling.SchellingConfiguration.RoomValue;
 
 public class SchellingModel extends Game<RoomState> {
 
-    Queue<Cell<RoomState>> emptyRooms;
-    Queue<Cell<RoomState>> initialEmptyRooms;
+    // TODO: Instead of a List, I would like to randomize the empty room that a family choose to get
+    // In this case a hashmap could serve better for improving access time
+    // But how to get a random value from a hashmap or hashset?
+    List<Cell<RoomState>> emptyRooms;
+    List<Cell<RoomState>> initialEmptyRooms;
 
     public SchellingModel(int rows, int columns) {
         super(rows, columns);
 
-        // collectInitialEmptyRooms
-        this.emptyRooms = new ArrayDeque<>();
+        this.emptyRooms = new LinkedList<>();
+        this.initialEmptyRooms = new LinkedList<>();
+
         for (ArrayList<Cell<RoomState>> cell : this.grid.cells()) {
             for (Cell<RoomState> c : cell) {
-                if (c.getState().getRoomValue().equals(RoomValue.EMPTY))
-                    // TODO: I don't need new cells, only to track the (x,y) pos
+                if (c.getState().getRoomValue().equals(RoomValue.EMPTY)) {
                     emptyRooms.add(c.clone());
+                }
             }
         }
+        this.initialEmptyRooms = new LinkedList<>(emptyRooms);
 
-        this.initialEmptyRooms = new ArrayDeque<>(emptyRooms);
     }
 
     public SchellingModel(SchellingModel model) {
         super(model);
-        this.emptyRooms = new ArrayDeque<>(model.emptyRooms);
+        this.emptyRooms = new LinkedList<>(model.emptyRooms);
+        this.initialEmptyRooms = new LinkedList<>(model.initialEmptyRooms);
     }
 
     public SchellingModel() {
@@ -56,7 +58,6 @@ public class SchellingModel extends Game<RoomState> {
     @Override
     public RoomState cellNextState(Cell<RoomState> cell) {
         assert(!cell.getState().getRoomValue().equals(RoomValue.EMPTY));
-        //  1-Check if I want to change of neighborhood and there is a free place
         long undesirableNeighborCount = this.getCellNeighbors(cell).stream().filter(neighbor -> {
             RoomValue neighborType = neighbor.getState().getRoomValue();
             RoomValue myType = cell.getState().getRoomValue();
@@ -65,7 +66,6 @@ public class SchellingModel extends Game<RoomState> {
         }).count();
 
         if (undesirableNeighborCount > SchellingConfiguration.threshold && !emptyRooms.isEmpty()) {
-            // IF EMPTY SPACE TO MOVE -> Actual cell become empty
             return new RoomState(RoomValue.EMPTY);
         }
 
@@ -73,9 +73,24 @@ public class SchellingModel extends Game<RoomState> {
     }
 
     public void nextState(SchellingModel newState) {
-        // 1. Build non empty room list
-        List<Cell<RoomState>> busyRooms = new ArrayList<>();
+        List<Cell<RoomState>> busyRooms = collectBusyRooms();
+        for (Cell<RoomState> busyCell : busyRooms) {
+            RoomState nextCellState = this.cellNextState(busyCell); // empty or the same
+            if (nextCellState.getRoomValue().equals(RoomValue.EMPTY)) {
+                Cell<RoomState> newFamilyHouse = chooseEmptyRoom();
+                newState.grid.setCellState(newFamilyHouse.getX(), newFamilyHouse.getY(), busyCell.getState().clone());
+                busyCell.setState(nextCellState.clone());
+                this.emptyRooms.add(busyCell.clone());
+            }
+            newState.grid.setCellState(busyCell.getX(), busyCell.getY(), nextCellState.clone());
+        }
+
+        this.grid = newState.grid;
+    }
+
+    private ArrayList<Cell<RoomState>> collectBusyRooms() {
         // TODO: This could be an iterator over all members of grid.
+        ArrayList<Cell<RoomState>> busyRooms = new ArrayList<>();
         for  (int row = 0; row < getRows(); row++) {
             for (int column = 0; column < getColumns(); column++) {
                 Cell<RoomState> cell = this.grid.getCell(row, column);
@@ -84,25 +99,14 @@ public class SchellingModel extends Game<RoomState> {
                 }
             }
         }
-        // Update them
-        for (Cell<RoomState> busyCell : busyRooms) {
-            RoomState nextCellState = this.cellNextState(busyCell); // empty or the same
-            if (nextCellState.getRoomValue().equals(RoomValue.EMPTY)) {
-                // Pick an empty room
-                Cell<RoomState> newFamilyHouse = this.emptyRooms.poll();
-                // Warning because poll is not verified to be not null,
-                //but If I have a RoomValue.Empty, that means I already did the
-                // verification on the cellNextState, this should be improved.
-                //newFamilyHouse.setState(busyCell.getState());
-                newState.grid.setCellState(newFamilyHouse.getX(), newFamilyHouse.getY(), busyCell.getState().clone());
-                busyCell.setState(nextCellState.clone());
-                this.emptyRooms.add(busyCell.clone());
-            }
-            newState.grid.setCellState(busyCell.getX(), busyCell.getY(), nextCellState.clone());
-        }
 
-        // TODO: I only need to update the grid, not the whole game.
-        this.grid = newState.grid;
+        return busyRooms;
+    }
+
+    private Cell<RoomState> chooseEmptyRoom() {
+        Random random = new Random();
+        int index = random.nextInt(this.emptyRooms.size());
+        return this.emptyRooms.remove(index);
     }
 
     @Override
@@ -123,10 +127,9 @@ public class SchellingModel extends Game<RoomState> {
         };
     }
 
-    // TODO: For the game restart I need to save the emptyRooms on an initialState.
     @Override
     public void restart() {
         this.grid = new Grid<>(this.initialGrid);
-        this.emptyRooms = new ArrayDeque<>(this.initialEmptyRooms);
+        this.emptyRooms = new LinkedList<>(this.initialEmptyRooms);
     }
 }
